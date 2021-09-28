ac78baa1063572777b157c44a50127b8aae2c2b1
wkh0430 <b10611035@ntu.edu.tw>
2021-09-28 23:33:51 +0800

    add file

---
+++ HW01B10611035.cpp
@@ -0,0 +1,26 @@
+//================================================================================================
+//  PROGRAMMER : 王凱弘
+//  DATE       : 2021/09/28
+//  FILENAME   : HW01B10611035.cpp
+//  DESCRIPTION: This is a program to compute the sum and average of three floating-point numbers.
+//================================================================================================
+
+#include <iostream>
+#include <iomanip> // Header file needed to use stream manipulators
+using namespace std;
+
+int main(){
+    double answer1; // Store the sum of the three numbers in this global variable 
+    double answer2; // Store the average of the three numbers in this global variable
+    double a, b, c; // store three input numbers 
+
+    cin >> a >> b >> c; // input the values of three variables
+
+    answer1 = a + b + c; // plus the values of three variables
+    answer2 = answer1 / 3; // calculate the average of three variables
+
+    cout << "The sum of the three numbers is " << fixed << setprecision(1) << answer1 << "." << endl; // print answer1 
+    cout << "The average of the three numbers is " << fixed << setprecision(1) << answer2 << "." << endl; // print answer2
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <iomanip> // Header file needed to use stream manipulators
 using namespace std;
 
 int main(){
-    cout << "hello world";
+    double a = 3.0;
+    float b = 2.0;
+    cout << fixed << setprecision(1) << a << b;
 
     return 0;
     
