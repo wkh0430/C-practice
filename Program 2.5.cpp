0d0ffffdf4da91af1fed1abfeb031932d092805b
wkh0430 <b10611035@ntu.edu.tw>
2021-09-28 22:27:29 +0800

    add file

---
+++ Program 2.5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout << "\nData Type Bytes"
+ << "\n--------- -----"
+ << "\nint " << sizeof(int)
+ << "\nchar " << sizeof(char)
+ << "\nbool " << sizeof(bool)
+ << '\n';
+ return 0;
+}
---
+++ testint.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    
+    a = 3;
+
+    cout << (a + 6);
+
+    return 0;
+
+}
