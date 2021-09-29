407a678c5863f75713e6583fd81fe946795a3b0e
wkh0430 <b10611035@ntu.edu.tw>
2021-09-29 08:55:50 +0800

    add global variable

--- HW01B_B10611035.cpp
+++ HW01B_B10611035.cpp
@@ -6,12 +6,12 @@
 //================================================================================================
 
 #include <iostream>
-#include <iomanip> // Header file needed to use stream manipulators
 using namespace std;
 
-int main(){
     double answer1; // Store the sum of the three numbers in this global variable 
     double answer2; // Store the average of the three numbers in this global variable
+    
+int main(){
     double a, b, c; // store three input numbers 
 
     cin >> a >> b >> c; // input the values of three variables
@@ -19,8 +19,8 @@ int main(){
     answer1 = a + b + c; // plus the values of three variables
     answer2 = (a + b + c) / 3.0; // calculate the average of three variables
 
-    cout << "The sum of the three numbers is " << fixed << setprecision(1) << answer1 << "." << endl; // print answer1 to one decimal place 
-    cout << "The average of the three numbers is " << fixed << setprecision(1) << answer2 << "." << endl; // print answer2 to one decimal place
+    cout << "The sum of the three numbers is " << answer1 << "." << endl; // print answer1 
+    cout << "The average of the three numbers is " << answer2 << "." << endl; // print answer2
 
     return 0;
 }
