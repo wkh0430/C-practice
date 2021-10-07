e185af2cb6566418c6213b8522994cc96d936baa
wkh0430 <b10611035@ntu.edu.tw>
2021-10-07 16:55:03 +0800

    add file

--- lab03-4_b10611035.cpp
+++ lab03-4_b10611035.cpp
@@ -2,20 +2,23 @@
 #include <cmath>
 using namespace std;
 
-int answer1 = 0;
+int answer1;
 
 int main()
 {
     double a, b, c;
-    int judge;
+    int disc;
     double root1, root2;
 
     cout << "This program calculate the root of a\n   quadratic equation of the from \n            2\n          ax + bx + c = 0\n"
-         << "Please enter the value of a. b. and c: ";
+         << "Please enter the value for a. b. and c: ";
     cin >> a >> b >> c;
 
     if (a == 0.0 && b == 0.0)
+    {
         cout << "The equation is degenerate and has no roots.\n";
+        answer1 = 0;
+    }
 
     else if (a == 0.0)
     {
@@ -25,23 +28,27 @@ int main()
 
     else
     {
-        judge = b * b - 4 * a * c;
+        disc = b * b - 4 * a * c;
 
-        if (judge > 0)
+        if (disc > 0)
         {
-            judge = sqrt(judge);
-            root1 = (-b + judge)/(2 * a);
-            root2 = (-b - judge)/(2 * a);
+            disc = sqrt(disc);
+            root1 = (-b + disc)/(2 * a);
+            root2 = (-b - disc)/(2 * a);
             cout << "The two real roots are " << root1 << " and " << root2 << endl;
+            answer1 = 2;
         }
 
-        else if (judge == 0)
+        else if (disc == 0)
+        {
             cout << "Both roots are equal to " << -b / (2 * a) << endl;
-
+            answer1 = 1;
+        }
         else
+        {
             cout << "Both roots are imaginary.\n";
-
-        answer1 = 2;
+            answer1 = 2;
+        }
     }
 
     return 0;
